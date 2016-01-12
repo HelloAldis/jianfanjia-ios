585a35b86131444ce412c881bbc63ee6c1ef6530
karosLi <karosli1314@gmail.com>
2016-01-12 18:48:41 +0800

    add image

--- jianfanjia/jianfanjia/Screen/Me/Info/BindPhoneViewController.m
+++ jianfanjia/jianfanjia/Screen/Me/Info/BindPhoneViewController.m
@@ -50,7 +50,10 @@
 }
 
 - (void)initNavi {
-    [self initLeftBackInNav];
+    self.navigationController.navigationBarHidden = NO;
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"icon_delete"] style:UIBarButtonItemStylePlain target:self action:@selector(onClickBack)];
+    self.navigationItem.leftBarButtonItem = item;
+    
     self.navigationController.navigationBarHidden = NO;
     [self.navigationController.navigationBar setBackgroundImage:[[UIImage alloc] init] forBarPosition:UIBarPositionAny barMetrics:UIBarMetricsDefault];
     self.navigationController.navigationBar.shadowImage = [[UIImage alloc] init];
