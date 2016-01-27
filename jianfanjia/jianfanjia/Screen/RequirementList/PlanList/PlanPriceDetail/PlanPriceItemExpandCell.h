e6f146cd3ed89d5f44febe67d2e39f6401ed3834
karosLi <karosli1314@gmail.com>
2016-01-27 17:00:33 +0800

    merge new plan price detail screen from designer to user

---
+++ jianfanjia/jianfanjia/Screen/RequirementList/PlanList/PlanPriceDetail/PlanPriceItemExpandCell.h
@@ -0,0 +1,15 @@
+//
+//  PlanPriceItemExpandCell.h
+//  jianfanjia-designer
+//
+//  Created by JYZ on 16/1/27.
+//  Copyright © 2016年 JYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlanPriceItemExpandCell : UITableViewCell
+
+- (void)initWithPriceItem:(PriceItem *)priceItem;
+
+@end
