741731f388b94c18a17ea916456fe8b3d57b6a34
karosLi <karosli1314@gmail.com>
2016-02-24 19:13:40 +0800

    update version to 2.3.0

--- jianfanjia/jianfanjia/AppDelegate.h
+++ jianfanjia/jianfanjia/AppDelegate.h
@@ -9,12 +9,12 @@
 #import <UIKit/UIKit.h>
 
 // 个推开发者网站中申请App时注册的AppId、AppKey、AppSecret
-#ifdef DEBUG
-// Dev
-#define kGtAppId           @"t9nLEYqbWI7HUX2QkE79v2"
-#define kGtAppKey          @"m2fZOPk3fy5pgq6szkabb7"
-#define kGtAppSecret       @"cO8DCexdK69ug14SeeIbg7"
-#else
+//#ifdef DEBUG
+//// Dev
+//#define kGtAppId           @"t9nLEYqbWI7HUX2QkE79v2"
+//#define kGtAppKey          @"m2fZOPk3fy5pgq6szkabb7"
+//#define kGtAppSecret       @"cO8DCexdK69ug14SeeIbg7"
+//#else
 // Test
 #define kGtAppId           @"SLKdGK8YIr9wns6NPEL8v8"
 #define kGtAppKey          @"O3oCpGEAVp7NjP67JbMPt5"
@@ -24,7 +24,7 @@
 //#define kGtAppId           @"YZV748rCe89l8CfZ7dtIF9"
 //#define kGtAppKey          @"8GJ1XgPOL9ArriE8xOJDK8"
 //#define kGtAppSecret       @"sJtpmFxS0a5sA30Au3iI36"
-#endif
+//#endif
 
 #define kUMengAppKey       @"55ffb334e0f55a84d500247f"
 
