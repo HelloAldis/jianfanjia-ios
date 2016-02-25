dc11decc39bcede7922073216647f3a6bb12f9fd
karosLi <karosli1314@gmail.com>
2016-02-25 17:25:04 +0800

    fix build error

---
+++ common/API/Request/BeautifulImageHomePageProtocol.h
@@ -0,0 +1,31 @@
+//
+//  BeautifulImageHomePageProtocol.h
+//  jianfanjia
+//
+//  Created by Karos on 16/2/22.
+//  Copyright © 2016年 JYZ. All rights reserved.
+//
+
+#ifndef BeautifulImageHomePageProtocol_h
+#define BeautifulImageHomePageProtocol_h
+
+@protocol BeautifulImageHomePageDataManagerProtocol <NSObject>
+
+@required
+- (NSMutableArray *)beautifulImages;
+- (NSInteger)loadMoreBeautifulImages;
+- (NSInteger)total;
+
+@end
+
+@protocol BeautifulImageHomePageLoadMoreRequestProtocol <NSObject>
+
+@required
+- (NSNumber *)from;
+- (void)setFrom:(NSNumber *)from;
+- (NSNumber *)limit;
+- (void)setLimit:(NSNumber *)limit;
+
+@end
+
+#endif /* BeautifulImageHomePageProtocol_h */
