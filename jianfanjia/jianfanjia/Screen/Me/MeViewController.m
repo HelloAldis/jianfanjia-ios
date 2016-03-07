966a0d5a0503661df266b525f27678ebe9128ead
karosLi <karosli1314@gmail.com>
2016-03-07 14:21:57 +0800

    update my screen

--- jianfanjia/jianfanjia/Screen/Me/MeViewController.m
+++ jianfanjia/jianfanjia/Screen/Me/MeViewController.m
@@ -12,23 +12,21 @@
 #import "ViewControllerContainer.h"
 #import "MyFavoriateViewController.h"
 #import "CustomerServiceViewController.h"
+#import "FeedbackViewController.h"
 
 @interface MeViewController ()
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIImageView *userImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *userThumnail;
 @property (weak, nonatomic) IBOutlet UILabel *lblUsername;
 @property (weak, nonatomic) IBOutlet UILabel *lblPhone;
-@property (weak, nonatomic) IBOutlet UIButton *btnNotification;
-@property (weak, nonatomic) IBOutlet UIButton *btnFavoriateDesigner;
-@property (weak, nonatomic) IBOutlet UIButton *btnSetting;
-@property (weak, nonatomic) IBOutlet UIImageView *userThumnail;
+@property (weak, nonatomic) IBOutlet UILabel *lblCache;
 
 @property (assign, nonatomic) CGFloat originUserImageHeight;
 @property (assign, nonatomic) BOOL isTabbarhide;
 
 @end
-
 
 @implementation MeViewController
 
@@ -40,6 +38,7 @@
     [self.userThumnail setBorder:1 andColor:[[UIColor whiteColor] CGColor]];
     self.originUserImageHeight = CGRectGetHeight(self.userImageView.frame);
     self.automaticallyAdjustsScrollViewInsets = NO;
+    [self updateCache];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -61,13 +60,6 @@
     if (self.isTabbarhide) {
         [self showTabbar];
     }
-    
-    @weakify(self);
-    [[NotificationDataManager shared] subscribeAllUnreadCount:^(id value) {
-        @strongify(self);
-        self.btnNotification.badgeValue = [value intValue] > 0 ? [value stringValue] : nil;
-        [self.btnNotification adjustBadgeToCloseText];
-    }];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
@@ -91,30 +83,65 @@
 }
 
 #pragma mark - user action
-- (IBAction)onClickNotification:(id)sender {
-    [ViewControllerContainer showReminder:nil refreshBlock:nil];
-}
-
-- (IBAction)onClickFavoriateDesigner:(id)sender {
-    MyFavoriateViewController *v = [[MyFavoriateViewController alloc] initWithNibName:nil bundle:nil];
-    [self.navigationController pushViewController:v animated:YES];
-}
-
 - (IBAction)onTapUserImageView:(id)sender {
     UserInfoViewController *v = [[UserInfoViewController alloc] initWithNibName:nil bundle:nil];
     [self.navigationController pushViewController:v animated:YES];
 }
 
-- (IBAction)onClickContactCustomerService:(id)sender {
-//    CustomerServiceViewController *v = [[CustomerServiceViewController alloc] init];
-//    [self.navigationController pushViewController:[CustomerServiceViewController instance] animated:YES];
-//    
+- (IBAction)onClickNotification:(id)sender {
+    [ViewControllerContainer showReminder:nil refreshBlock:nil];
+}
+
+- (IBAction)onClickFavoriate:(id)sender {
+    MyFavoriateViewController *v = [[MyFavoriateViewController alloc] initWithNibName:nil bundle:nil];
+    [self.navigationController pushViewController:v animated:YES];
+}
+
+- (IBAction)onClickComment:(id)sender {
     [ViewControllerContainer showMyComments];
 }
 
-- (IBAction)onClickSetting:(id)sender {
+- (IBAction)onClickAcountBind:(id)sender {
+    [ViewControllerContainer showAccountBind];
+}
+
+- (IBAction)onClickClearCache:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"确定清空缓存？" message:nil preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        //Do nothing
+    }];
+    
+    @weakify(self)
+    UIAlertAction *done = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        @strongify(self);
+        YYImageCache *cache = [YYWebImageManager sharedManager].cache;
+        [cache.memoryCache removeAllObjects];
+        [cache.diskCache removeAllObjects];
+        [self updateCache];
+    }];
+    
+    [alert addAction:cancel];
+    [alert addAction:done];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (IBAction)onClickSuggestion:(id)sender {
+    FeedbackViewController *v = [[FeedbackViewController alloc] initWithNibName:nil bundle:nil];
+    [self.navigationController pushViewController:v animated:YES];
+}
+
+- (IBAction)onClickOnlineService:(id)sender {
+    [self.navigationController pushViewController:[CustomerServiceViewController instance] animated:YES];
+}
+
+- (IBAction)onClickMore:(id)sender {
     SettingViewController *v = [[SettingViewController alloc] init];
     [self.navigationController pushViewController:v animated:YES];
+}
+
+- (IBAction)onClickPhoneConsult:(id)sender {
+    [PhoneUtil call:@"4008515167"];
 }
 
 #pragma mark - scroll view  delegate 
@@ -140,6 +167,11 @@
             self.tabBarController.tabBar.frame = CGRectOffset(self.tabBarController.tabBar.frame, 0, -50);
         }];
     }
+}
+
+- (void)updateCache {
+    YYImageCache *cache = [YYWebImageManager sharedManager].cache;
+    self.lblCache.text = [@((cache.memoryCache.totalCost + cache.diskCache.totalCost) /8 ) humSizeString];
 }
 
 @end
