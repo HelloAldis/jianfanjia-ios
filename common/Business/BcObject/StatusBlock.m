a5984a620979e07e860ef896cbcea1bef4346489
karosLi <karosli1314@gmail.com>
2016-04-11 09:35:45 +0800

    add status pool

--- common/Business/BcObject/StatusBlock.m
+++ common/Business/BcObject/StatusBlock.m
@@ -10,12 +10,29 @@
 
 NSString * const kElseStatus = @"ElseStatus";
 
+static NSMutableDictionary *statusObjPool = nil;
+
 @implementation StatusBlock
 
++ (void)initialize {
+    if ([self class] == [StatusBlock class]) {
+        statusObjPool = [NSMutableDictionary dictionary];
+    }
+}
+
 + (StatusBlock *)match:(NSString *)status action:(StatusBlockAction)actionBlock {
-    StatusBlock *statusBlock = [[StatusBlock alloc] init];
-    statusBlock.status = status;
-    statusBlock.actionBlock = actionBlock;
+    StatusBlock *statusBlock = [statusObjPool objectForKey:status];
+    
+    if (statusBlock) {
+        statusBlock.actionBlock = actionBlock;
+        return statusBlock;
+    } else {
+        statusBlock = [[StatusBlock alloc] init];
+        statusBlock.status = status;
+        statusBlock.actionBlock = actionBlock;
+        
+        [statusObjPool setValue:statusBlock forKey:status];
+    }
     
     return statusBlock;
 }
@@ -23,6 +40,13 @@ NSString * const kElseStatus = @"ElseStatus";
 + (void)matchReqt:(NSString *)status actions:(NSArray <StatusBlock *>*)actions {
     [actions enumerateObjectsUsingBlock:^(StatusBlock * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         if ([status isEqualToString:obj.status]) {
+            if (obj.actionBlock) {
+                obj.actionBlock();
+            }
+            *stop = YES;
+        }
+        
+        if (idx == actions.count - 1 && [kElseStatus isEqualToString:obj.status]) {
             if (obj.actionBlock) {
                 obj.actionBlock();
             }
