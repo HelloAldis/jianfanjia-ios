734cd7f1672ccae5b58bdcd2654cc589d95a18ae
karosLi <karosli1314@gmail.com>
2016-04-18 14:32:49 +0800

    add supervisor app

---
+++ jianfanjia-supervisor/jianfanjia-supervisor/Pods.h
@@ -0,0 +1,22 @@
+//
+//  pods.h
+//  jianfanjia
+//
+//  Created by JYZ on 15/8/24.
+//  Copyright (c) 2015年 JYZ. All rights reserved.
+//
+
+#ifndef jianfanjia_pods_h
+#define jianfanjia_pods_h
+
+#import "AFNetworking.h"
+#import "AFNetworkActivityLogger.h"
+#import "CocoaLumberjack.h"
+#import "MJRefresh.h"
+#import "MBProgressHUD.h"
+#import "GVUserDefaults.h"
+#import "ReactiveCocoa.h"
+#import "AFNetworkActivityIndicatorManager.h"
+#import "YYWebImage.h"
+
+#endif
