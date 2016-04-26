a579d0a9da15c7dfb690fce3a2021a702c44c91d
karosLi <karosli1314@gmail.com>
2016-04-26 17:33:52 +0800

    add toast util

---
+++ common/Util/ToastUtil.h
@@ -0,0 +1,17 @@
+//
+//  ToastUtil.h
+//  jianfanjia
+//
+//  Created by Karos on 16/4/26.
+//  Copyright © 2016年 JYZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^ToastTapBlock) (NSMutableDictionary *userInfo);
+
+@interface ToastUtil : NSObject
+
++ (void)showNotification:(Notification *)notification tapBlock:(ToastTapBlock)tapBlock;
+
+@end
