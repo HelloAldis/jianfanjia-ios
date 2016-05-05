861c7d0d79c89dd570126f5a0b0eedec88afdf13
karosLi <karosli1314@gmail.com>
2016-05-05 15:55:28 +0800

    display tag for designer info

--- jianfanjia/jianfanjia/Screen/Designer/DesignerInfoCell.m
+++ jianfanjia/jianfanjia/Screen/Designer/DesignerInfoCell.m
@@ -11,6 +11,9 @@
 @interface DesignerInfoCell ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *designerImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *imgHandings;
+@property (weak, nonatomic) IBOutlet UIView *tagView;
+@property (weak, nonatomic) IBOutlet UILabel *lblTag;
 @property (weak, nonatomic) IBOutlet UIImageView *vImageView;
 @property (weak, nonatomic) IBOutlet UILabel *lblViewCount;
 @property (weak, nonatomic) IBOutlet UILabel *lblProductCount;
@@ -28,7 +31,9 @@
 - (void)awakeFromNib {
     [self.btnAdd setCornerRadius:5];
     [self.btnAdd setBorder:2 andColor:[kThemeColor CGColor]];
-    [self.designerImageView setCornerRadius:30];
+    [self.designerImageView setCornerRadius:self.designerImageView.frame.size.width / 2.0];
+    [self.designerImageView setBorder:1 andColor:[UIColor whiteColor].CGColor];
+    [self.tagView setCornerRadius:self.tagView.frame.size.height / 2.0];
 }
 
 - (void)initWithDesigner:(Designer *)designer {
@@ -36,9 +41,11 @@
     
     [self.designerImageView setUserImageWithId:designer.imageid];
     self.lblDesignerName.text = designer.username;
-    self.lblViewCount.text = [designer.view_count stringValue];
+    self.lblViewCount.text = [designer.view_count humCountString];
     self.lblProductCount.text = [designer.authed_product_count stringValue];
     self.lblOrderCount.text = [designer.order_count stringValue];
+    self.lblTag.text = [DesignerBusiness designerTagTextByArr:designer.tags];
+    self.tagView.bgColor = [DesignerBusiness designerTagColorByArr:designer.tags];
     
     [self refreshAdd];
     
@@ -47,6 +54,10 @@
     UIImage *empty = [UIImage imageNamed:@"star_middle_empty"];
     [DesignerBusiness setStars:self.starts withStar:star fullStar:full emptyStar:empty];
     [DesignerBusiness setV:self.vImageView withAuthType:designer.auth_type];
+    
+    BOOL isJiangXin = [DesignerBusiness containsJiangXinDingZhiTag:designer.tags];
+    self.imgHandings.hidden = !isJiangXin;
+    self.tagView.hidden = isJiangXin;
 }
 
 - (void)refreshAdd {
