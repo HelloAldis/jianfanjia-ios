56fa532c733639aaa67f593e4d3e964b725a85b9
karosLi <karosli1314@gmail.com>
2016-05-31 17:29:47 +0800

    refine reset  / verify screen

--- jianfanjia-designer/jianfanjia-designer/Screen/Login/ResetPassViewController.m
+++ jianfanjia-designer/jianfanjia-designer/Screen/Login/ResetPassViewController.m
@@ -14,7 +14,6 @@
 @property (weak, nonatomic) IBOutlet UITextField *fldPhone;
 @property (weak, nonatomic) IBOutlet UITextField *fldPassword;
 @property (weak, nonatomic) IBOutlet UIButton *btnNext;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraint;
 
 @end
 
@@ -23,7 +22,6 @@
 #pragma mark - life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self initTransparentNavBar:UIBarStyleBlack];
 
     [[[self.fldPhone.rac_textSignal filterNonDigit:^BOOL{
         return YES;
@@ -49,24 +47,33 @@
           [self.btnNext enableBgColor:[x boolValue]];
       }];
     
+    self.title = @"忘记密码";
     [self initLeftBackInNav];
+    [self.fldPhone setCornerRadius:5];
+    [self.fldPassword setCornerRadius:5];
     [self.btnNext setCornerRadius:5];
     self.btnNext.enabled = NO;
+    
+    [self setLeftPadding:self.fldPhone withImage:[UIImage imageNamed:@"icon_account_phone"]];
+    [self setLeftPadding:self.fldPassword withImage:[UIImage imageNamed:@"icon_account_pwd"]];
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    
-    if (kIs35inchScreen) {
-        self.constraint.constant = 84;
-    } else {
-        self.constraint.constant = (kScreenHeight - 400)/2;
-    }
-}
 #pragma mark - UI
+- (void)setLeftPadding:(UITextField *)textField withImage:(UIImage *)image {
+    CGRect frame = textField.frame;
+    frame.origin.x = 0;
+    frame.origin.y = 0;
+    frame.size.width = 60;
+    UIImageView *leftview = [[UIImageView alloc] initWithFrame:frame];
+    leftview.image = image;
+    leftview.contentMode = UIViewContentModeCenter;
+    textField.leftViewMode = UITextFieldViewModeAlways;
+    textField.leftView = leftview;
+}
 
 #pragma mark - user actions
 - (IBAction)onClickNext:(id)sender {
+    [self.view endEditing:YES];
     [DataManager shared].signupPagePhone = self.fldPhone.text;
     [DataManager shared].signupPagePass = self.fldPassword.text;
     
@@ -82,7 +89,5 @@
         
     }];
 }
-
-
 
 @end
