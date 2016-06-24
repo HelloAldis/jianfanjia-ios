62a8f6cf0443ef77f889236e485500efb0705985
karosLi <karosli1314@gmail.com>
2016-06-24 19:00:56 +0800

    remove comment

--- jianfanjia/jianfanjia/Screen/DecDiary/DiaryDetail/DiaryMessageCell.m
+++ jianfanjia/jianfanjia/Screen/DecDiary/DiaryDetail/DiaryMessageCell.m
@@ -44,19 +44,4 @@
     }
 }
 
-//- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-//    [[self nextResponder] touchesBegan:touches withEvent:event];
-//    [super touchesBegan:touches withEvent:event];
-//}
-//
-//- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-////    [[self nextResponder] touchesMoved:touches withEvent:event];
-//    [super touchesMoved:touches withEvent:event];
-//}
-//
-//- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-//    [[self nextResponder] touchesEnded:touches withEvent:event];
-//    [super touchesMoved:touches withEvent:event];
-//}
-
 @end
