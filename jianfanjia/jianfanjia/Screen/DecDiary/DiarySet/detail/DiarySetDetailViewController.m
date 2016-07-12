a3fac5abbb29fcc84d54e5c35d642a85456e31fe
karosLi <karosli1314@gmail.com>
2016-07-12 16:16:56 +0800

    refine

--- jianfanjia/jianfanjia/Screen/DecDiary/DiarySet/detail/DiarySetDetailViewController.m
+++ jianfanjia/jianfanjia/Screen/DecDiary/DiarySet/detail/DiarySetDetailViewController.m
@@ -101,7 +101,7 @@ static NSString *DecDiaryStatusCellIdentifier = @"DecDiary1StatusCell";
     self.dataManager = [[DiarySetDetailDataManager alloc] init];
     self.automaticallyAdjustsScrollViewInsets = NO;
     self.tableView.contentInset = UIEdgeInsetsMake(kNavWithStatusBarHeight, 0, 120, 0);
-    self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(kNavWithStatusBarHeight, 0, 0, 0);;
+    self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, 0, 0);;
     self.tableView.estimatedRowHeight = 300;
     [self.tableView registerNib:[UINib nibWithNibName:DiarySetAvtarInfoCellIdentifier bundle:nil] forCellReuseIdentifier:DiarySetAvtarInfoCellIdentifier];
     [self.tableView registerNib:[UINib nibWithNibName:DecDiaryStatusCellIdentifier bundle:nil] forCellReuseIdentifier:DecDiaryStatusCellIdentifier];
