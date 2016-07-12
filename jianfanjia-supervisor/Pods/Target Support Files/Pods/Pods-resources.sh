d2f2afaaa048534593b9050e6243d6c80937f860
karosLi <karosli1314@gmail.com>
2016-07-12 16:11:57 +0800

    apply new photo cropper

--- jianfanjia-supervisor/Pods/Target Support Files/Pods/Pods-resources.sh
+++ jianfanjia-supervisor/Pods/Target Support Files/Pods/Pods-resources.sh
@@ -59,11 +59,11 @@ install_resource()
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "MJRefresh/MJRefresh/MJRefresh.bundle"
-  install_resource "RSKImageCropper/RSKImageCropper/RSKImageCropperStrings.bundle"
+  install_resource "${BUILT_PRODUCTS_DIR}/TOCropViewControllerBundle.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "MJRefresh/MJRefresh/MJRefresh.bundle"
-  install_resource "RSKImageCropper/RSKImageCropper/RSKImageCropperStrings.bundle"
+  install_resource "${BUILT_PRODUCTS_DIR}/TOCropViewControllerBundle.bundle"
 fi
 
 mkdir -p "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
