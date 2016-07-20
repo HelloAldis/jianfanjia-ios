2def315b252b9d8c0c75a277f2ca85428dcce206
karosLi <karosli1314@gmail.com>
2016-07-20 10:22:05 +0800

    resolve compile error

--- common/UIkit/WebView/BaseWebViewController.m
+++ common/UIkit/WebView/BaseWebViewController.m
@@ -148,8 +148,7 @@ static NSString *MessageModel = @"jianfanjia";
 
 #pragma mark - user action
 - (void)onClickShare {
-    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:self.articleImgUrl]]];
-    [[ShareManager shared] share:self topic:self.topic image:image ? image : [UIImage imageNamed:@"about_logo"] title:![self.webView.title isEmpty] ? self.webView.title : DefaultTitle description:self.articleDescription ? self.articleDescription : @"我在使用 #简繁家# 的App，业内一线设计师为您量身打造房间，比传统装修便宜20%，让你一手轻松掌控装修全过程。" targetLink:self.webView.URL.absoluteString delegate:self];
+    //subclass should override
 }
 
 - (void)onClickBack {
