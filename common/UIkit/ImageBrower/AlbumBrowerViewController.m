423188a97963c202fcc78094671d23472b13b288
karosLi <karosli1314@gmail.com>
2016-07-20 13:02:10 +0800

    replace to high quanlity image

--- common/UIkit/ImageBrower/AlbumBrowerViewController.m
+++ common/UIkit/ImageBrower/AlbumBrowerViewController.m
@@ -119,7 +119,7 @@
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 60;
+    return 80;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
