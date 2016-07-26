3a96f04102af7c39e79dbd1741e2144af0edbd48
karosLi <karosli1314@gmail.com>
2016-07-26 18:12:50 +0800

    route specified controller according to url

--- jianfanjia/jianfanjia/General/Manager/Router/MyDeepLinkRouter.m
+++ jianfanjia/jianfanjia/General/Manager/Router/MyDeepLinkRouter.m
@@ -7,6 +7,8 @@
 //
 
 #import "MyDeepLinkRouter.h"
+#import "ViewControllerContainer.h"
+#import "WebViewController.h"
 
 @implementation MyDeepLinkRouter
 
@@ -19,12 +21,31 @@
 }
 
 - (void)configure {
-    self[@"*"] = ^(DPLDeepLink *link) {
-        [[[UIAlertView alloc] initWithTitle:@"逗比"
-                                    message:link.URL.absoluteString
-                                   delegate:nil
-                          cancelButtonTitle:NSLocalizedString(@"OK", nil)
-                          otherButtonTitles:nil] show];
+    //jianfanjia://m.jianfanjia.com/jianfanjiaapp/diaryset?diarySetid=577104e90d135f8d2d493eba
+    self[@".*\\.jianfanjia\\.com/jianfanjiaapp/diaryset"] = ^(DPLDeepLink *link) {
+        NSString *diarySetId = link[@"diarySetid"];
+        GetDiarySetDetail *request = [[GetDiarySetDetail alloc] init];
+        request.diarySetid = diarySetId;
+        [API getDiarySetDetail:request success:^{
+            NSMutableDictionary *dic = [DataManager shared].data;
+            DiarySet *diarySet = [[DiarySet alloc] initWith:dic];
+            [ViewControllerContainer showDiarySetDetail:diarySet fromNewDiarySet:NO];
+        } failure:^{
+            
+        } networkError:^{
+            
+        }];
+    };
+    
+    //jianfanjia://m.jianfanjia.com/jianfanjiaapp/webview?url=http://devm.jianfanjia.com/weixin/jian/
+    self[@".*\\.jianfanjia\\.com/jianfanjiaapp/webview"] = ^(DPLDeepLink *link) {
+        NSString *url = link[@"url"];
+        [WebViewController show:[ViewControllerContainer getCurrentTopController] withUrl:url shareTopic:@"Link"];
+    };
+    
+    //default
+    self[@".*\\.jianfanjia\\.com/jianfanjiaapp/.*"] = ^(DPLDeepLink *link) {
+        
     };
 }
 
