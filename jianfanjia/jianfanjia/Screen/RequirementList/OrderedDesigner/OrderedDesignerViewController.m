1c9a2eac74b0420589eecf2526dc1f75be1d4aa5
karosLi <karosli1314@gmail.com>
2016-08-11 14:55:58 +0800

    remove action button for ordered designer

--- jianfanjia/jianfanjia/Screen/RequirementList/OrderedDesigner/OrderedDesignerViewController.m
+++ jianfanjia/jianfanjia/Screen/RequirementList/OrderedDesigner/OrderedDesignerViewController.m
@@ -113,6 +113,9 @@ static NSString *PlanExpiredIdentifier = @"PlanExpiredCell";
        [PlanExpired action:^{
             height = 96;
         }],
+       [PlanDesignerDeclined action:^{
+            height = 96;
+        }],
       ]];
     
     return height;
