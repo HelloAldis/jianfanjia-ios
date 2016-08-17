676867433eb4e304bc76d9f9e8dc1fd3d01d9fb9
karosLi <karosli1314@gmail.com>
2016-08-17 11:40:36 +0800

    use same ui of request plan for requirement list screen

--- jianfanjia/jianfanjia/Screen/RequirementList/RequirementListViewController.m
+++ jianfanjia/jianfanjia/Screen/RequirementList/RequirementListViewController.m
@@ -11,15 +11,13 @@
 #import "RequirementCell.h"
 #import "ViewControllerContainer.h"
 #import "SuccessAlertViewController.h"
+#import "RequestPlanViewController.h"
 
 static NSString *RequirementCellIdentifier = @"RequirementCell";
 
 @interface RequirementListViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (weak, nonatomic) IBOutlet UIScrollView *reqScrollView;
-@property (weak, nonatomic) IBOutlet UITextField *fldNickName;
-@property (weak, nonatomic) IBOutlet UITextField *fldPhone;
-@property (weak, nonatomic) IBOutlet UIButton *btnReq;
+@property (strong, nonatomic) RequestPlanViewController *requestPlanVC;
 
 @property (strong, nonatomic) RequirementDataManager *dataManager;
 
@@ -35,31 +33,9 @@ static NSString *RequirementCellIdentifier = @"RequirementCell";
     [self initNav];
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-
-    @weakify(self);
-    [self jfj_subscribeKeyboardWithAnimations:^(CGRect keyboardRect, BOOL isShowing) {
-        @strongify(self);
-        if (isShowing) {
-            CGFloat keyboardHeight = keyboardRect.size.height;
-            self.reqScrollView.contentInset = UIEdgeInsetsMake(0, 0, keyboardHeight + kTabBarHeight, 0);
-            self.reqScrollView.contentOffset = CGPointMake(0, keyboardHeight - kTabBarHeight - 55);
-        } else {
-            self.reqScrollView.contentInset = UIEdgeInsetsMake(0, 0, kTabBarHeight, 0);
-            self.reqScrollView.contentOffset = CGPointMake(0, 0);
-        }
-    } completion:nil];
-}
-
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     [self refreshRequirements:NO];
-}
-
-- (void)viewDidDisappear:(BOOL)animated {
-    [super viewDidDisappear:animated];
-    [self jfj_unsubscribeKeyboard];
 }
 
 #pragma mark - init ui
@@ -82,69 +58,17 @@ static NSString *RequirementCellIdentifier = @"RequirementCell";
         [self refreshRequirements:NO];
     }];
     
-    [[[self.fldPhone.rac_textSignal filterNonDigit:^BOOL{
-        return YES;
-    }] length:^NSInteger{
-        return kPhoneLength;
-    }] subscribeNext:^(id x) {
-        @strongify(self);
-        self.fldPhone.text = x;
-    }];
-    
-    [[RACSignal
-      combineLatest:@[self.fldPhone.rac_textSignal, self.fldNickName.rac_textSignal]
-      reduce:^(NSString *phone, NSString *nickName) {
-          return @([AccountBusiness validatePhone:phone] && ![nickName isEmpty]);
-      }] subscribeNext:^(id x) {
-          [self.btnReq enableBgColor:[x boolValue]];
-      }];
-    
-    self.reqScrollView.contentInset = UIEdgeInsetsMake(0, 0, kTabBarHeight, 0);
-    self.reqScrollView.scrollIndicatorInsets = UIEdgeInsetsMake(kNavWithStatusBarHeight, 0, kTabBarHeight, 0);
-    [self.btnReq setCornerRadius:5];
-    [self.fldNickName setCornerRadius:5];
-    [self.fldPhone setCornerRadius:5];
-    
-    [self setLeftPadding:self.fldNickName withImage:[UIImage imageNamed:@"icon_requirement_nickname"]];
-    [self setLeftPadding:self.fldPhone withImage:[UIImage imageNamed:@"icon_requirement_phone"]];
-    
+    self.requestPlanVC = [[RequestPlanViewController alloc] init];
+    self.requestPlanVC.view.frame = CGRectMake(0, 0, kScreenWidth, kScreenHeight);
+ 
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleLogout) name:kLogoutNotification object:nil];
-}
-
-- (void)setLeftPadding:(UITextField *)textField withImage:(UIImage *)image {
-    CGRect frame = textField.frame;
-    frame.origin.x = 0;
-    frame.origin.y = 0;
-    frame.size.width = 60;
-    UIView *leftview = [[UIView alloc] init];
-    leftview.frame = frame;
-    
-    frame.size.width = frame.size.height;
-    UIView *leftImgview = [[UIView alloc] init];
-    leftImgview.frame = frame;
-    leftImgview.backgroundColor = [UIColor colorWithR:0xE7 g:0xEC b:0xEF];
-    
-    frame.size.width = 20;
-    frame.size.height = frame.size.width;
-    frame.origin.x = (leftImgview.frame.size.width - frame.size.width) / 2.0;
-    frame.origin.y = (leftImgview.frame.size.height - frame.size.height) / 2.0;
-    UIImageView *leftImg = [[UIImageView alloc] initWithFrame:frame];
-    leftImg.image = image;
-    leftImg.contentMode = UIViewContentModeScaleAspectFit;
-    
-    [leftImgview addSubview:leftImg];
-    [leftview addSubview:leftImgview];
-    textField.leftViewMode = UITextFieldViewModeAlways;
-    textField.leftView = leftview;
 }
 
 - (void)showRequirementList:(BOOL)show {
     if (show) {
-        self.reqScrollView.hidden = YES;
-        self.tableView.hidden = NO;
+        [self.requestPlanVC.view removeFromSuperview];
     } else {
-        self.reqScrollView.hidden = NO;
-        self.tableView.hidden = YES;
+        [self.view addSubview:self.requestPlanVC.view];
     }
 }
 
@@ -239,26 +163,6 @@ static NSString *RequirementCellIdentifier = @"RequirementCell";
 #pragma mark - user action
 - (void)onClickCustomerService {
     [PhoneUtil call:@"咨询热线" phone:kConsultPhone];
-}
-
-- (IBAction)onClickRequest:(id)sender {
-    AddAngelUser *request = [[AddAngelUser alloc] init];
-    request.name = self.fldNickName.text;
-    request.phone = self.fldPhone.text;
-    request.district = kAngelUserDistrictRequirement;
-    
-    [HUDUtil showWait];
-    [API addAngelUser:request success:^{
-        [self.view endEditing:YES];
-        self.fldPhone.text = nil;
-        self.fldNickName.text = nil;
-        [self.btnReq enableBgColor:NO];
-        [SuccessAlertViewController presentAlert:@"申请成功" msg:@"我们的工作人员将在24小时之内与您联系，请保持电话畅通" ok:nil];
-    } failure:^{
-        
-    } networkError:^{
-        
-    }];
 }
 
 @end
